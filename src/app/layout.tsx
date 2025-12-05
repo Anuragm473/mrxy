da3e63328ff783d59bf7d66d6054452fec5b8783
Anurag473 <anurag47mishra@gmail.com>
2025-12-05 15:11:25 +0530

    unauthroze j

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
 import { CartProvider } from "@/context/CartContext";
-import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -52,9 +51,8 @@ export default function RootLayout({
         {/* 👇 Prevents dark mode auto inversion on mobile */}
         <meta name="color-scheme" content="light only" />
         <meta name="theme-color" content="#000000" />
-        <Script
+        <script
         src="https://checkout.razorpay.com/v1/checkout.js"
-        onError={() => alert('Failed to load payment system')}
       />
       </head>
       <body
